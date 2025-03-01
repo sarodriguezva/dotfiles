89b381a6a30856f8d30a81cee2a8007d090c2e93
sarodriguezva <sarodriguezva@unal.edu.co>
2025-03-01 16:59:49 -0500

    feat(nvim): 💄 enhance appeareance

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,22 @@
 return {
   "projekt0n/github-nvim-theme",
-  lazy = false, -- Para asegurarte de que el tema esté disponible al inicio
+  lazy = false,
   priority = 1000,
   config = function()
     require("github-theme").setup({
       options = {
-        transparent = false, -- Activa fondo transparente si lo deseas
-        terminal_colors = true,
+        transparent = true,
+        terminal_colors = false,
+      },
+      groups = {
+        all = {
+          NeoTreeFileName = { fg = "#d29922", bold = true },
+          NeoTreeDirectoryName = { fg = "#58a6ff" },
+          NeoTreeFileNameOpened = { fg = "#ff7b72", bold = true },
+          NeoTreeCursorLine = { bg = "#30363d" },
+        },
       },
     })
+    vim.cmd("colorscheme github_dark_colorblind")
   end,
 }
